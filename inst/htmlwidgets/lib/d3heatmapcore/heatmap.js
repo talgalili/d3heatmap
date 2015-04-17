e6f00eced59e1a1ea365d8699001c466c84c2b67
Joe Cheng <joe@rstudio.org>
2015-04-17 16:17:30 -0700

    Keep opposing axes highlighted

--- inst/htmlwidgets/lib/d3heatmapcore/heatmap.js
+++ inst/htmlwidgets/lib/d3heatmapcore/heatmap.js
@@ -386,12 +386,19 @@ function heatmap(selector, data, options) {
     });
     el.classed('heatmap-hover', hasX || hasY);
     
-    el.selectAll('.axis g.tick').classed('highlight', false);
     if (hasX) {
-      d3.select(el.selectAll('.xaxis g.tick')[0][x]).classed('highlight', true);
+      el.selectAll('.xaxis g.tick').classed('highlight', function(d, i) {
+        return i == x;
+      });
+    } else {
+      el.selectAll('.xaxis g.tick').classed('highlight', true);
     }
     if (hasY) {
-      d3.select(el.selectAll('.yaxis g.tick')[0][y]).classed('highlight', true);
+      el.selectAll('.yaxis g.tick').classed('highlight', function(d, i) {
+        return i == y;
+      });
+    } else {
+      el.selectAll('.yaxis g.tick').classed('highlight', true);
     }
   }
   
