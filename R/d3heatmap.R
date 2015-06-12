07bb8f65152b79fe4d12df05851ad6f8cd628f0b
Tal Galili <tal.galili@gmail.com>
2015-06-12 23:44:56 +0300

    Added some examples of how to use this new d3heatmap code. This should later be removed or moved to examples.

--- R/d3heatmap.R
+++ R/d3heatmap.R
@@ -486,3 +486,87 @@ renderD3heatmap <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
   shinyRenderWidget(expr, d3heatmapOutput, env, quoted = TRUE)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(FALSE) {
+
+  # basic example of using dendextend:
+  
+  x <- mtcars[c(2:4,7),1:4]
+  library(dendextend)
+  row_dend2 <- x %>% dist %>% hclust %>% as.dendrogram %>%
+    color_branches(k = 3)
+  col_dend2 <- x %>% t %>% dist %>% hclust %>% as.dendrogram %>%
+    color_branches(k = 2)
+  library(d3heatmap)
+  d3heatmap(x, Rowv = row_dend2, Colv = col_dend2) # Works!
+  
+  row_dend2 <- x %>% dist %>% hclust %>% as.dendrogram %>%
+    color_branches(k = 3) %>% 
+    set("branches_lwd", c(4,1)) %>%    
+    set("branches_lty", c(1,1,3))
+  plot(row_dend2)
+  # for now, d3heatmap still ignores line type and width:
+  d3heatmap(x, Rowv = row_dend2, Colv = col_dend2) # Works!
+  
+  
+  
+  # various examples  
+  library(d3heatmap)
+  d3heatmap(scale(mtcars), colors = "Greens", theme = "dark")
+  d3heatmap(scale(mtcars), cluster = FALSE)
+  
+  d3heatmap(scale(mtcars))
+  d3heatmap(scale(mtcars), cluster = FALSE)
+  d3heatmap(scale(mtcars), Colv = FALSE)
+  d3heatmap(scale(mtcars), Rowv = FALSE)
+  d3heatmap(scale(mtcars), dendrogram = "row")
+  d3heatmap(scale(mtcars), dendrogram = "col")
+  d3heatmap(scale(mtcars), dendrogram = "none") # like cluster = FALSE
+  
+  x <- mtcars[c(2:4,7),1:4]
+  d3heatmap(x)
+  d3heatmap(x, cluster = FALSE)
+  
+  d3heatmap(x, scale = "row")
+  scale(x)
+  d3heatmap(scale(x), cluster = FALSE)
+  d3heatmap(x, scale = "column", cluster = FALSE)
+  d3heatmap(x, scale = "row", cluster = FALSE)
+  
+  heatmap(scale(mtcars[1:4,1:4]), Rowv = NA, Colv = NA)
+  
+  
+  library(dendextend)
+  d3heatmap(x)
+  # gives the same results - yay:
+  row_dend <- x %>% dist %>% hclust %>% as.dendrogram # %>% plot
+  d3heatmap(x, Rowv = row_dend)
+  row_dend2 <- x %>% dist %>% hclust %>% as.dendrogram %>%
+    color_branches(k = 2)
+  plot(row_dend2)
+  d3heatmap:::dendToTree(row_dend2[[2]][[2]])
+  d3heatmap(x, Rowv = row_dend2) # Works!
+  
+  # Next step!
+  row_dend3 <- x %>% dist %>% hclust %>% as.dendrogram %>%
+    set("branches_lwd", c(4,1)) %>%    
+    set("branches_lty", c(1,1,3)) %>%  
+    set("branches_col", c("gold","grey","blue", "red")) 
+  # set("branches_col", c(1,2,3)) # TODO: this doesn't work - needs to be fixed...
+  plot(row_dend3) # line width and line type are still ignored.
+  d3heatmap(x, Rowv = row_dend3) 
+  
+}
