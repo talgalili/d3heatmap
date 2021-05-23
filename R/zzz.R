c8a3c64a951d6e0fc33c5aebb71e4cb7aff91e29
Tal Galili <tal.galili@gmail.com>
2021-05-23 08:37:57 +0300

    Add startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,37 @@
+#' @importFrom utils packageDescription
+NULL
+
+.onAttach <- function(lib, pkg, ...) {
+  packageStartupMessage(d3heatmapWelcomeMessage())
+}
+
+#
+#
+
+
+d3heatmapWelcomeMessage <- function() {
+  # library(utils)
+  
+  paste0(
+    "\n",
+    "======================\n",
+    "Welcome to d3heatmap version ", utils::packageDescription("d3heatmap")$Version, "\n",
+    # "\n",
+    "\n",
+    "Type citation('d3heatmap') for how to cite the package.\n",
+    "Type ?d3heatmap for the main documentation.\n",
+    "\n",
+    "The github page is: https://github.com/talgalili/d3heatmap/\n",
+    "Please submit your suggestions and bug-reports at: https://github.com/talgalili/d3heatmap/issues\n",
+    "You may ask questions at stackoverflow, use the r and d3heatmap tags: \n\t https://stackoverflow.com/questions/tagged/d3heatmap\n",
+    # "\n",
+    # "\tTo suppress this message use:  ", "suppressPackageStartupMessages(library(d3heatmap))\n",
+    "======================\n"
+  )
+}
+
+
+
 # devtools::use_code_of_conduct()
 
 # When all is done, run:
